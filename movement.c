991fa9b4ac2a13af95a69f23bd4ef2788a2887f8
AminaKhalid <aminakhalid4@hotmail.com>
2017-04-17 15:22:03 +0100

    Moves done by Amina

--- movement.c
+++ movement.c
@@ -1,71 +1,88 @@
 //function for moving players
-void movePlayers(int p, int row, int column);
+void movePlayers(int i, int row, int column);
 {	
 	int option;
 	//player is on the top left corner
-	if((player[p].row == 0)&&(player[p].column == 0))
+	if ((player[i].position->row == 0) && (player[i].position->column == 0))
 	{	
 		printf("\nPlayer position: top left\n");
 		//the options to move
 		printf("Press 1 to move right or press 2 to move down\n");
 		scanf("%d", &option);
-		
+
 		if (option == 1)
 		{
-			printf("%s will move right.\n", players[p].Name);
-			players[p].column++; //moving column forward
+			printf("you will move right.\n");
+			//pointers for moving on the board
+			player[i].position = board[player[i].position->row][player[i].position->column].right;
+			//print new position of player
+			printf("Your new position is [%d, %d] ", player[i].position->row, player[i].position->column);
 		}
 		else if(option == 2)
 		{
-			printf("%s will move down.\n", players[p].Name);
-			players[p].row++; //moving row forward
+			printf("you will move down.\n");
+			//pointers for moving on the board
+			player[i].position = board[player[i].position->row][player[i].position->column].down;
+			//print new position of player
+			printf("Your new position is [%d, %d] ", player[i].position->row, player[i].position->column);
 		}
-		modValues(p, row, column) //function to change the player capabilities depending on their choice of movement
+		
+		newValues(i, row, column); //function to change the player capabilities depending on their choice of movement
 	}
 	//player is on the top right corner
-	else if((player[p].row == 0)&&(player[p].column == boardSize-1))
+	else if((player[i].position->row == 0) && (player[i].position->column == boardSize - 1))
 	{
 		printf("\nPlayer position: top right\n");
-		//options to move
+		
 		printf("Press 3 to move left or press 2 to move down: \n");
 		scanf("%d", &option);
 		
 		if(option == 3)
 		{
-			printf("%s will move left.\n", players[p].Name);
-			players[p].column--; //moving column back
+			printf("you will move left.\n");
+			//pointers for moving on the board
+			player[i].position = board[player[i].position->row][player[i].position->column].left;
+			//print new position of player
+			printf("Your new position is [%d, %d] ", player[i].position->row, player[i].position->column);
 		}
 		else if(option == 2)
 		{
-			printf("%s will move down.\n", players[p].Name);
-			players[p].row++; //moving row forward
+			printf("you will move down.\n");
+			//pointers for moving on the board
+			player[i].position = board[player[i].position->row][player[i].position->column].down;
+			//print new position of player
+			printf("Your new position is [%d, %d] ", player[i].position->row, player[i].position->column);
 		}
 		
-		modValues(p, row, column)
+		newValues(i, row, column); //function to change the player capabilities depending on their choice of movement
 	}
 	//player is on the bottom left corner
-	else if((player[p].row == boardSize-1)&&(player[p].column == 0))
+	else if((player[i].position->row == boardSize - 1) && (player[i].position->column == 0))
 	{
 		printf("\nPlayer position: bottom left\n");
-		//moving options
+		
 		printf("Press 1 to move right or press 4 to move up: ");
 		scanf("%d", &option);
 		
 		if(option == 1)
 		{
-			printf("%s will move right.\n", players[p].Name);
-			players[p].column++; //moving column forward
+			printf("you will move right.\n");
+			//pointers for moving on the board
+			player[i].position = board[player[i].position->row][player[i].position->column].right;
+			printf("Your new position is [%d, %d] ", player[i].position->row, player[i].position->column);
 		}
 		else if(option == 4)
 		{
-			printf("%s will move up.\n", players[p].Name);
-			players[p].row--; //moving row back
+			printf("you will move up.\n");
+			//pointers for moving on the board
+			player[i].position = board[player[i].position->row][player[i].position->column].up;
+			printf("Your new position is [%d, %d] ", player[i].position->row, player[i].position->column);
 		}
 		
-		modValues(p, row, column)
+		newValues(i, row, column); //function to change the player capabilities depending on their choice of movement
 	}
 	//player is on the bottom right corner
-	else if((player[p].row == boardSize-1)&&(player[p].column == boardSize-1))
+	else if((player[i].position->row == boardSize - 1) && (player[i].position->column == boardSize - 1))
 	{
 		printf("\nPlayer position: bottom right\n");
 		
@@ -74,19 +91,23 @@ void movePlayers(int p, int row, int column);
 		
 		if(option == 3)
 		{
-			printf("%s will move left.\n", players[p].Name);
-			players[p].column--; //moving column back
+			printf("you will move left.\n");
+			//pointers for moving on the board
+			player[i].position = board[player[i].position->row][player[i].position->column].left;
+			printf("Your new position is [%d, %d] ", player[i].position->row, player[i].position->column);
 		}
 		else if(option == 4)
 		{
-			printf("%s will move up.\n", players[Pp].Name);
-			players[p].row--; //moving row back
+			printf("you will move up.\n");
+			//pointers for moving on the board
+			player[i].position = board[player[i].position->row][player[i].position->column].up;
+			printf("Your new position is [%d, %d] ", player[i].position->row, player[i].position->column);
 		}
 		
-		modValues(p, row, column)
+		newValues(i, row, column); //function to change the player capabilities depending on their choice of movement
 	}
 	//player is in the one of the top rows (no corners)
-	else if(player[p].row == 0)
+	else if((player[i].position->row == 0) && (player[i].position->column != 0 ))
 	{	
 		printf("\nPlayer is in the top row.\n");
 		
@@ -95,24 +116,30 @@ void movePlayers(int p, int row, int column);
 		
 		if(option == 3)
 		{
-			printf("%s will move left.\n", players[p].Name);
-			players[p].column--; //moving column back
+			printf("you will move left.\n");
+			//pointers for moving on the board
+			player[i].position = board[player[i].position->row][player[i].position->column].left;
+			printf("Your new position is [%d, %d] ", player[i].position->row, player[i].position->column);
 		}
 		else if(option == 2)
 		{
-			printf("%s will move down.\n", players[p].Name);
-			players[p].row++; //moving row forward
+			printf("you will move down.\n");
+			//pointers for moving on the board
+			player[i].position = board[player[i].position->row][player[i].position->column].down;
+			printf("Your new position is [%d, %d] ", player[i].position->row, player[i].position->column);
 		}
 		else if(option == 1)
 		{
-			printf("%s will move right.\n", players[p].Name);
-			players[p].column++; //moving column forward
+			printf("you will move right.\n");
+			//pointers for moving on the board
+			player[i].position = board[player[i].position->row][player[i].position->column].right;
+			printf("Your new position is [%d, %d] ", player[i].position->row, player[i].position->column);
 		}
 		
-		modValues(p, row, column)
+		newValues(i, row, column); //function to change the player capabilities depending on their choice of movement
 	}
 	//player is on the first column (no corners)
-	else if(player[p].column == 0)
+	else if((player[i].position->column == 0) && (player[i].position->row != 0))
 	{
 		printf("\nPlayer is in the left column\n");
 		
@@ -121,24 +148,30 @@ void movePlayers(int p, int row, int column);
 		
 		if(option == 4)
 		{
-			printf("%s will move up.\n", players[p].Name);
-			players[p].row--; //moving row back
+			printf("you will move up.\n");
+			//pointers for moving on the board
+			player[i].position = board[player[i].position->row][player[i].position->column].up;
+			printf("Your new position is [%d, %d] ", player[i].position->row, player[i].position->column);
 		}
 		else if(option == 2)
 		{
-			printf("%s will move down.\n", players[p].Name);
-			players[p].row++; //moving row forward
+			printf("you will move down.\n");
+			//pointers for moving on the board
+			player[i].position = board[player[i].position->row][player[i].position->column].down;
+			printf("Your new position is [%d, %d] ", player[i].position->row, player[i].position->column);
 		}
 		else if(option == 1)
 		{
-			printf("%s will move right.\n", players[p].Name);
-			players[p].column++; //moving column forward
+			printf("you will move right.\n");
+			//pointers for moving on the board
+			player[i].position = board[player[i].position->row][player[i].position->column].right;
+			printf("Your new position is [%d, %d] ", player[i].position->row, player[i].position->column);
 		}
 		
-		modValues(p, row, column)
+		newValues(i, row, column); //function to change the player capabilities depending on their choice of movement
 	}
 	//player is on one of the bottom rows (no corners)
-	else if(player[p].row == boardSize-1)
+	else if((player[i].position->row == boardSize - 1) && (player[i].position->column != 0 ))
 	{
 		printf("\nPlayer is in the bottom row\n");
 		
@@ -147,24 +180,30 @@ void movePlayers(int p, int row, int column);
 		
 		if(option == 3)
 		{
-			printf("%s will move left.\n", players[p].Name);
-			players[p].column--; //moving column back
+			printf("you will move left.\n");
+			//pointers for moving on the board
+			player[i].position = board[player[i].position->row][player[i].position->column].left;
+			printf("Your new position is [%d, %d] ", player[i].position->row, player[i].position->column);
 		}
 		else if(option == 4)
 		{
-			printf("%s will move up.\n", players[p].Name);
-			players[p].row--; //moving row back
+			printf("you will move up.\n");
+			//pointers for moving on the board
+			player[i].position = board[player[i].position->row][player[i].position->column].up;
+			printf("Your new position is [%d, %d] ", player[i].position->row, player[i].position->column);
 		}
 		else if(option == 1)
 		{
-			printf("%s will move right.\n", players[p].Name);
-			players[p].column++; //moving column forward
+			printf("you will move right.\n");
+			//pointers for moving on the board
+			player[i].position = board[player[i].position->row][player[i].position->column].right;
+			printf("Your new position is [%d, %d] ", player[i].position->row, player[i].position->column);
 		}
 		
-		modValues(p, row, column)
+		newValues(i, row, column); //function to change the player capabilities depending on their choice of movement
 	}
 	//player in in one of the last columns (no corners)
-	else if(player[p].column == boardSize-1)
+	else if((player[i].position->column = boardSize - 1) && (player[i].position->row != 0 ))
 	{	
 		printf("\nPlayer is in right column.\n");
 		
@@ -173,27 +212,33 @@ void movePlayers(int p, int row, int column);
 		
 		if(option == 4)
 		{
-			printf("%s will move up.\n", players[p].Name);
-			players[p].row--; //moving row back
+			printf("you will move up.\n");
+			//pointers for moving on the board
+			player[i].position = board[player[i].position->row][player[i].position->column].up;
+			printf("Your new position is [%d, %d] ", player[i].position->row, player[i].position->column);
 		}
 		if(option == 3)
 		{
-			printf("%s will move left.\n", players[p].Name);
-			players[Pp].column--; //moving column back
+			printf("you will move left.\n");
+			//pointers for moving on the board
+			player[i].position = board[player[i].position->row][player[i].position->column].left;
+			printf("Your new position is [%d, %d] ", player[i].position->row, player[i].position->column);
 		}
 		else if(option == 2)
 		{
-			printf("%s will move down.\n", players[p].Name);
-			players[p].row++; //moving row forward
+			printf("you will move down.\n");
+			//pointers for moving on the board
+			player[i].position = board[player[i].position->row][player[i].position->column].down;
+			printf("Your new position is [%d, %d] ", player[i].position->row, player[i].position->column);
 		}
 		
-		modValues(p, row, column)
+		newValues(i, row, column); //function to change the player capabilities depending on their choice of movement
 	}
 	//player is in the centre of the board
-	else if((player[p].row != 0) && (player[p].column != 0) && (player[p].column != 0) (player[p].row != BoardSize - 1))
+	else if((player[i].position->row != 0) && (player[i].position->row != boardSize - 1) && (player[i].position->column != 0) && (player[i].position->column != 0))
 	{
 		printf("\nPlayer position: centre of the board");
-		//options to move
+		
 		printf("\nPress 1 to move right: ");
 		printf("\nPress 2 to move down: ");
 		printf("\nPress 3 to move left: ");
@@ -202,61 +247,69 @@ void movePlayers(int p, int row, int column);
 		
 		if(option == 1)
 		{
-			printf("%s will move right.\n", players[p].Name);
-			players[p].column++; //moving column forward
+			printf("you will move right.\n");
+			//pointers for moving on the board
+			player[i].position = board[player[i].position->row][player[i].position->column].right;
+			printf("Your new position is [%d, %d] ", player[i].position->row, player[i].position->column);
 		}
 		else if(option == 2)
 		{
-			printf("%s will move down.\n", players[p].Name);
-			players[p].row++; //moving row forward
+			printf("you will move down.\n");
+			//pointers for moving on the board
+			player[i].position = board[player[i].position->row][player[i].position->column].down;
+			printf("Your new position is [%d, %d] ", player[i].position->row, player[i].position->column);
 		}
 		if(option == 3)
 		{
-			printf("%s will move left.\n", players[p].Name);
-			players[p].column--; //moving column back
+			printf("you will move left.\n");
+			//pointers for moving on the board
+			player[i].position = board[player[i].position->row][player[i].position->column].left;
+			printf("Your new position is [%d, %d] ", player[i].position->row, player[i].position->column);
 		}
 		else if(option == 4)
 		{
-			printf("%s will move up.\n", players[p].Name);
-			players[p].row--; //moving row back
+			printf("you will move up.\n");
+			//pointers for moving on the board
+			player[i].position = board[player[i].position->row][player[i].position->column].up;
+			printf("Your new position is [%d, %d] ", player[i].position->row, player[i].position->column);
 		}
 		
-		playerVal(p, row, column)
+		newValues(i, row, column); //function to change the player capabilities depending on their choice of movement
 	}
 	
 }
 
-void playerVal(int p, int row, int column)
+void playerVal(int i, int row, int column)
 {
 
 	//if the player moves to a Hill slot:
-	if(strcmp(board[player[p].row][player[p].column].SlotType, "Hill") == 0)
+	if (player[i].position->TypeOfSlot == 1)
 	{	
 		//If Dexterity < 50, then the player loses 10 Strength points
-		if (player[p].dexterity < 50 )
+		if (player[i].dexterity < 50 )
 		{
-			player[p].strength = player[p].strength - 10;
+			player[i].strength = player[i].strength - 10;
 		}
 
 		//If Dexterity >= 60, then the player gains 10 Strength points
-		else if (player[p].dexterity >= 60)
+		else if (player[i].dexterity >= 60)
 		{
-			players[p].strength = players[p].strength + 10;
+			players[i].strength = players[i].strength + 10;
 		}
 	}
 	//if the player moves to a City slot:
-	else if(strcmp(board[player[p].row][player[p].column].SlotType, "City") == 0)
+	else if (player[i].position->TypeOfSlot == 2)
 	{	
 		//If Smartness > 60, then the player gains 10 Magic Skills points
-		if (player[p].smartness > 60)
+		if (player[i].smartness > 60)
 		{
-			player[p].magic_skills = player[p].magic_skills + 10;
+			player[i].magic_skills = player[i].magic_skills + 10;
 		}
 		
 		//If Smartness <=50, then the player loses 10 Dexterity points.
-		else if (player[p].smartness <= 50)
+		else if (player[i].smartness <= 50)
 		{
-			player[p].dexterity = player[p].dexterity - 10;
+			player[i].dexterity = player[i].dexterity - 10;
 		}
 	}
 }
